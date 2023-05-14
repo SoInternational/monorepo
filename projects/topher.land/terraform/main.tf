92d2430a745ea46afb29e037479e36bfff870875
Chris Ackerman <chris@topher.land>
2023-05-14 15:21:13 -0700

    chore: migrated tf state/lock

--- projects/topher.land/terraform/main.tf
+++ projects/topher.land/terraform/main.tf
@@ -10,8 +10,8 @@ terraform {
 
   backend "s3" {
     region         = "us-east-1"
-    dynamodb_table = "tfstate-main-topherland"
-    bucket         = "tfstate-main-topherland"
+    dynamodb_table = "tfstate-lock"
+    bucket         = "tfstate-433680868508"
     key            = "topher.land"
     encrypt        = true
   }
